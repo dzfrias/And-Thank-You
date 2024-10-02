e75ea403e3a97250987b4c4981b4a402fbce25db
Diego Frias <styx5242@gmail.com>
2024-10-02 16:35:59 -0700

    Fix basic enemy AI
    Will now properly turn around when at the edge of a platform, and when
    hitting a wall.

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -1,37 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
+[RequireComponent(typeof(Ground), typeof(Collider2D))]
 public class EnemyController : MonoBehaviour, IMovementController
 {
-    private bool _canMove;
+    private Ground _ground;
+    private Collider2D _collider;
+    private float _movement = 1f;
+
+    private void Start()
+    {
+        _ground = GetComponent<Ground>();
+        _collider = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (!_ground.onGround) return;
+
+        var groundCollider = _ground.ground.GetComponent<Collider2D>();
+        if ((_collider.Right() > groundCollider.Right() && _movement > 0) || (_collider.Left() < groundCollider.Left() && _movement < 0))
+        {
+            Flip();
+        }
+    }
 
     public float GetMovement()
     {
-        if (_canMove)
-        {
-            return (transform.forward.x > 0) ? 1 : -1;
-        }
-        else
-        {
-            return 0;
-        }
-        
+        if (!_ground.onGround) return 0;
+        return _movement;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        transform.localRotation = Quaternion.Euler(0, ((transform.localRotation.y + 180) % 360), 0);
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector2 normal = collision.GetContact(i).normal;
+            if (normal.x != 0)
+            {
+                _movement = normal.x > 0 ? 1 : -1;
+            }
+        }
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void Flip()
     {
-        _canMove = true;
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        _canMove = false;
+        _movement *= -1;
     }
 }
